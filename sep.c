fc350bdc3eb81df8e186cdec6751fda539e211b5
PsychoticPendulum <80325726+PsychoticPendulum@users.noreply.github.com>
2022-01-03 12:08:24 +0100

    Initial Upload
    Draw a separating line in the terminal

---
+++ sep.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
+
+// Windows
+#include <windows.h>
+int get_width() {
+    CONSOLE_SCREEN_BUFFER_INFO csbi;
+    int columns;
+
+    GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
+    columns = csbi.srWindow.Right - csbi.srWindow.Left + 1;
+
+    return columns;
+}
+
+// Linux
+// #include <sys/ioctl.h>
+/*int getWidth() {
+	struct winsize w;
+	ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
+	return w.ws_col;
+}*/
+
+int main(int argc, char *argv[]) {
+    int option;
+    char *sym = "-";
+    int lines = 1;
+
+    while ((option = getopt(argc, argv, "l:s:")) != -1) {
+        switch (option) {
+            case 'l':
+                lines = atoi(optarg);
+                break;
+            case 's':
+                sym = optarg;
+                break;
+        }
+    }
+
+    int w = get_width();
+    int length = strlen(sym);
+    int remainder = w % length - 1;
+    for (int i = 0; i < lines; i++) {
+		for (int j = 0; j < w / strlen(sym) - 1; j++) {
+			printf(sym);
+		}
+		for (int k = 0; k < remainder; k++) {
+			printf("%c", sym[k]);
+		}
+		printf("\n");
+	}
+
+    return EXIT_SUCCESS;
+}
